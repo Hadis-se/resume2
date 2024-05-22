13483b952f84675bce4ba02afe3f9c286c19b5e6
Hadis <sedaghat.hadis@gmail.com>
2024-05-21 21:54:10 -0400

    final commit

--- server/db/mongodb.mjs
+++ server/db/mongodb.mjs
@@ -1,39 +1,54 @@
-// const { MongoClient } = require('mongodb');
 
-import { MongoClient } from 'mongodb'
 
-let client = ''
-let users = ''
-let results = ''
+import { MongoClient } from 'mongodb';
 
-export const initMongoDb = async () => {
-    if (!client) {
-        client = new MongoClient(process.env.MONGODB_URL);
-        await client.connect();
-        users = client.db().collection('users');
-        users.createIndex( { "email": 1 }, { unique: true } )
-        results = client.db().collection('results');
-    }
-    return client
+
+
+
+
+
+
+ let client = '';
+ let users = '';
+ let results = '';  
+
+ export const initMongodb = async () => {
+   
+  if (!client) {
+    client = new MongoClient(process.env.MONGODB_URL);
+    await client.connect();
+     users = client.db().collection('users');
+     users.createIndex({email: 1}, {unique: true});
+     results = client.db().collection('results');
+  }
+  return client;
+ 
 }
-
-
 
 export const saveUsers = async (userPayload) => {
-    const result = await users.insertOne(userPayload);
-    return result;
+
+   const result = await users.insertOne(userPayload);
+   return result;
+   
+
+
 }
 
+export const findUserByEmail = async (email) => {
+  const result = await users.findOne({
+    email
+  });
+   return result;
 
-export const findUserByEmail= async (email) => {
-    const result = await users.findOne({
-        email
-    });
-    return result;
 }
 
 export const saveResults = async (resultPayload) => {
-    const result = await results.insertOne(resultPayload);
-    return result;
+
+  const result = await results.insertOne(resultPayload);
+  return result;
+  
+
+
 }
+
 
